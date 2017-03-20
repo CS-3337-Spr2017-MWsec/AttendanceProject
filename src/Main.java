4c605698d83206342facd4508cf47fb337e599c9
cesar714 <cesar_rojas714@icloud.com>
2017-03-20 04:10:50 -0700

    Added strings for parsing

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,16 @@
+import java.util.ArrayList;
 
 public class Main {
 
 	public static void main(String[] args) {
 
 		Administrator test = new Administrator("Name","Mr","Richards","email","richards","ilovejose");
-		
+		String student = "%B6048880000654321^PERSON/THREE^4912120000000000000000000000000?;6048880000419099=4912120303452468?";
+		String firstName;
+		String lastName;
+		String midName;
+		String id;
+		System.out.println("length of string: " + student.length());
 	}
 
 }
