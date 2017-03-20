15f1470d238436121c7a9758447ed8ecaf8d6fa7
cesar <cesar714>
2017-03-20 15:00:11 -0700

    added parsing of student example

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,11 @@ public class Main {
 		//example: student card scan
 		String student = "%B6048880000654321^PERSON/THREE 4912120000000000000000000000000?;6048880000419099=4912120303452468?";
 		
+		String trash;
+		String trash_2;
 		String firstName;
 		String lastName;
+		String trash_3;
 		String midName;
 		String id;
 		
@@ -18,15 +21,20 @@ public class Main {
 		String[] tokens = student.split(delims);
 		for (int i = 0; i < tokens.length; i++)
 			System.out.println(number++ + ") " + tokens[i]);
-		/* output:
-		 1) 
-		 2) B6048880000654321
-		 3) PERSON
-		 4) THREE
-		 5) 4912120000000000000000000000000
-		 6) 6048880000419099
-		 7) 4912120303452468
-			*/
+			trash = tokens[0];
+			trash_2 = tokens[1];
+			firstName = tokens[2];
+			lastName = tokens[3];
+			trash_3 = tokens[4];
+			id = tokens[5];
+		System.out.println("trash: " + trash);
+		System.out.println("trash_2: " + trash_2);
+		System.out.println("firstName: " + firstName);
+		System.out.println("lastName: " + lastName);
+		System.out.println("trash_3: " + trash_3);
+		System.out.println("id: " + id);
+		
+		
 	}
 
 }
