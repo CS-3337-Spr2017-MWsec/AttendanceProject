0df91f28396baf2ae68effb8f57ff328beb1cec6
cesar <cesar714>
2017-03-20 14:46:46 -0700

    added string parsing of a student sample

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,28 @@ public class Main {
 	public static void main(String[] args) {
 
 		Administrator test = new Administrator("Name","Mr","Richards","email","richards","ilovejose");
-		String student = "%B6048880000654321^PERSON/THREE^4912120000000000000000000000000?;6048880000419099=4912120303452468?";
+		//example: student card scan
+		String student = "%B6048880000654321^PERSON/THREE 4912120000000000000000000000000?;6048880000419099=4912120303452468?";
+		
 		String firstName;
 		String lastName;
 		String midName;
 		String id;
-		System.out.println("length of string: " + student.length());
+		
+		int number = 1;
+		String delims = "[%,/,?,=,;,^, ]+";
+		String[] tokens = student.split(delims);
+		for (int i = 0; i < tokens.length; i++)
+			System.out.println(number++ + ") " + tokens[i]);
+		/* output:
+		 1) 
+		 2) B6048880000654321
+		 3) PERSON
+		 4) THREE
+		 5) 4912120000000000000000000000000
+		 6) 6048880000419099
+		 7) 4912120303452468
+			*/
 	}
 
 }
