56bd55657775995d77c39677cddfb8417c9dff8c
d1a9x8u9 <daxu7803@gmail.com>
2017-04-29 22:05:44 -0700

    Updated AddStudentViewController.java

--- src/application/view/AddStudentViewController.java
+++ src/application/view/AddStudentViewController.java
@@ -1,7 +1,16 @@
 package application.view;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Optional;
+
 import application.Main;
+import application.classes.AttendanceRecord;
+import application.classes.Student;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
 
 public class AddStudentViewController {
@@ -20,7 +29,18 @@ public class AddStudentViewController {
 	private TextField email;
 	
 	@FXML
-	public void addStudentConfirmBt() {
-		System.out.println(firstName.getText() + " " + lastName.getText() + " " + CIN.getText() + " " + email.getText());
+	public void addStudentConfirmBt() throws IOException {
+		if(firstName.getText().isEmpty() || lastName.getText().isEmpty() || CIN.getText().isEmpty() || email.getText().isEmpty()) {
+			Alert alert = new Alert(AlertType.ERROR, "Please fill in all neccesary student information.");
+			Optional<ButtonType> result = alert.showAndWait();
+			 if (result.isPresent() && result.get() == ButtonType.OK) {
+			 }
+		}
+		else {
+			Student student = new Student(firstName.getText(),lastName.getText(),Integer.parseInt(CIN.getText()),email.getText());
+			main.currentCourse.students.add(student);
+			main.showHomeScene();
+			
+		}
 	}
 }
