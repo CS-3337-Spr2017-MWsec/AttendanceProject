2f20d2e9e930f7a18309e09a93af8a001caae371
Teky Alvarado <alvarado.teky@outlook.com>
2017-04-29 19:36:43 -0700

    Update

--- src/application/view/AddCourseViewController.java
+++ src/application/view/AddCourseViewController.java
@@ -1,18 +1,50 @@
 package application.view;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 import application.Main;
+import application.classes.Course;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class AddCourseViewController {
 	private Main main;
-	
-	@FXML void createCourseConfirmBt() {
-		/*	create a form in AddCourseView.fxml using labels and textfields
-			get information from the textfields from AddCourseView.fxml
-			String name = 
-			String number =
-			String days =
-			String time = 
-		
-		main.createCourse(name, number days, time);
-		*/
+
+	@FXML
+	TextField name;
+	@FXML
+	TextField number;
+	@FXML
+	TextField days;
+	@FXML
+	TextField time;
+
+	@FXML
+	void createCourseConfirmBt() throws FileNotFoundException {
+
+		Course currentCourse = new Course(name.getText(), number.getText(), days.getText(), time.getText());
+
+		PrintWriter pw = new PrintWriter(new File(
+				name.getText() + "_" + number.getText() + "_" + days.getText() + "_" + time.getText() + ".csv"));
+		StringBuilder sb = new StringBuilder();
+		sb.append("First Name");
+		sb.append(',');
+		sb.append("Last Name");
+		sb.append(',');
+		sb.append("Guardian Email");
+		sb.append(',');
+		sb.append("Id");
+		sb.append('\n');
+
+		pw.write(sb.toString());
+		pw.close();
+
+		name.clear();
+		number.clear();
+		days.clear();
+		time.clear();
 	}
+
 }
