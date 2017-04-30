86fd4ddc6311339f03ddbb58ccc4d8ac0651fd7d
d1a9x8u9 <daxu7803@gmail.com>
2017-04-29 19:16:49 -0700

    Add files via upload
    Created GUI

--- src/application/view/AddStudentViewController.java
+++ src/application/view/AddStudentViewController.java
@@ -1,11 +1,26 @@
 package application.view;
 
+import application.Main;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 public class AddStudentViewController {
+	private Main main;
 
 	@FXML
+	private TextField firstName;
+
+	@FXML
+	private TextField lastName;
+
+	@FXML
+	private TextField CIN;
+	
+	@FXML
+	private TextField email;
+	
+	@FXML
 	public void addStudentConfirmBt() {
-		
+		System.out.println(firstName.getText() + " " + lastName.getText() + " " + CIN.getText() + " " + email.getText());
 	}
 }
