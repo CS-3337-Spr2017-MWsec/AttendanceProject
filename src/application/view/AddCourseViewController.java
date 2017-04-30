f4cc673c10069e7004b8f61bb546becb79f79c29
Teky Alvarado <alvarado.teky@outlook.com>
2017-04-29 20:30:40 -0700

    update

--- src/application/view/AddCourseViewController.java
+++ src/application/view/AddCourseViewController.java
@@ -24,27 +24,10 @@ public class AddCourseViewController {
 	@FXML
 	void createCourseConfirmBt() throws FileNotFoundException {
 
-		Course currentCourse = new Course(name.getText(), number.getText(), days.getText(), time.getText());
-
-		PrintWriter pw = new PrintWriter(new File(
-				name.getText() + "_" + number.getText() + "_" + days.getText() + "_" + time.getText() + ".csv"));
-		StringBuilder sb = new StringBuilder();
-		sb.append("First Name");
-		sb.append(',');
-		sb.append("Last Name");
-		sb.append(',');
-		sb.append("Guardian Email");
-		sb.append(',');
-		sb.append("Id");
-		sb.append('\n');
-
-		pw.write(sb.toString());
-		pw.close();
-
-		name.clear();
-		number.clear();
-		days.clear();
-		time.clear();
+		main.createCourse(name.getText(), number.getText(), days.getText(), time.getText());
 	}
 
+	
+	
+	
 }
